963eb9db132bb16892dfaa3820923bfab865f5dc
jefree <jgarzon920429@gmail.com>
2015-01-14 14:01:52 -0500

    build complex example

--- public/js/main.js
+++ public/js/main.js
@@ -3,20 +3,45 @@ var game = new Phaser.Game(400, 400, Phaser.CANVAS, 'canvas', { preload: preload
 var sticks = []
 
 var initial_sticks = [
-  {x: 0, y: 0, angle: 90},
-  {x: 1, y: 1, angle: 0},
-  {x: 2, y: 0, angle: -90},
-  {x: 3, y: 1, angle: 0},
+  {x: 0, y: 2, angle: 60 },
+  {x: 0, y: 4, angle: 120 },
+  {x: 1, y: 3, angle: 0 },
+  
+  {x: 2, y: 0, angle: 60 },
+  {x: 3, y: 1, angle: 0 },
+  {x: 2, y: 2, angle: 120 },
+
+  {x: 2, y: 6, angle: 120 },
+  {x: 3, y: 5, angle: 0 },
+  {x: 2, y: 4, angle: 60 },
+
+  {x: 5, y: 3, angle: 0 },
+  {x: 4, y: 0, angle: -60 },
+  {x: 4, y: 2, angle: -120 },
+
+  {x: 4, y: 4, angle: -60 },
+  {x: 4, y: 6, angle: -120 },
+
+  {x: 6, y: 2, angle: -60 },
+  {x: 6, y: 4, angle: -120 },
+
 ]
 
 var solution = [
-  {x: 0, y: 0, angle: 90},
-  {x: 2, y: 0, angle: -90},
+  {x: 0, y: 0, angle: 30},
 ]
 
 function onStickClick(stick) {
   console.log('click on stick :)');
-  stick.alive = stick.visible = false;
+
+  if (stick.alive) {
+    stick.alive = false;
+    stick.alpha = 0.2;
+  }
+  else {
+    stick.alive = true;
+    stick.alpha = 1.0
+  }
 }
 
 function preload() {
@@ -24,7 +49,7 @@ function preload() {
 }
 
 function create() {
-  StickMapper.config(50, 50, 50, 40)
+  StickMapper.config(40, 25, 50, 40)
 
   initial_sticks.forEach(function(stick) {
     sticks.push(new Stick(stick.x, stick.y, stick.angle));
@@ -54,7 +79,6 @@ function update() {
   }
 
   if (temp_sticks.length > 0 && temp_sticks.length == founds) {
-    alert('Victory');
-    location.reload();
+    console.log('Victory');
   }
 }
